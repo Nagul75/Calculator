dc209b2cddd0fc168294d789298e439aad058727
Nagul75 <nagulk697@gmail.com>
2024-08-17 13:57:19 +0530

    finish visual part of calculator

---
+++ scripts/script.js
@@ -0,0 +1,79 @@
+let inputString = "";
+
+const parentDiv = document.querySelector("#calculator");
+parentDiv.addEventListener("click", (e) => {
+    let target = e.target;
+    switch(target.id)
+    {
+        case "one": inputString + "1";
+        break;
+        case "two": inputString + "2";
+        break;
+        case "three": inputString + "3";
+        break;
+        case "four": inputString + "4";
+        break;
+        case "five": inputString + "5";
+        break;
+        case "six": inputString + "6";
+        break;
+        case "seven": inputString + "7";
+        break;
+        case "eight": inputString + "8";
+        break;
+        case "nine": inputString + "9"
+        break;
+        case "zero": inputString + "0";
+        break;
+        case "+": inputString + "+";
+        break;
+        case "-": inputString + "-";
+        break;
+        case "/": inputString + "/";
+        break;
+        case "*": inputString + "*";
+        break;
+        default:
+            alert("INCORRECT INPUT");
+    }
+})
+
+
+
+
+
+function add(num1, num2)
+{
+    return num1 + num2;
+}
+
+function subtract(num1, num2)
+{
+    return num1 - num2;
+}
+
+function divide(num1, num2)
+{
+    return num1/num2;
+}
+
+function multiply(num1, num2)
+{
+    return num1 * num2;
+}
+
+function operate(num1, op, num2)
+{
+    let result;
+    switch(op)
+    {
+        case "+": result =  add(num1, num2);
+        break;
+        case "-": result = subtract(num1, num2);
+        break;
+        case "*": result = multiply(num1, num2);
+        break;
+        case "/": result = divide(num1, num2);
+    }
+    return result;
+}
